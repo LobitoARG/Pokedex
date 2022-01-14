52c536e030aac5262dc5a93d3da3694e42ef5aae
jonimanzini <gitlab.com/jonimanzini>
2022-01-14 00:13:48 -0300

    prueba de agregar clase

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { ENOTEMPTY } = require("constants");
 let fs = require("fs");
 let {readPokedex,saveNewPokemon} = require("./PokeFunctions.js")
 
@@ -6,6 +7,41 @@ let procesar = process.argv;
 let option2 = procesar[2]
 let getInfo3 = procesar[3]
 //let gerInfo4 = procesar[4]
+
+class Pokemon {
+    constructor({
+        id,
+        pokeName,
+        type: [],
+        weaknesses:  [],
+        evolutions: [], 
+    })
+    {
+        this.id=id;
+        this.pokeName= pokeName;
+        this.type;
+        this.weaknesses;
+        this.evolutions;
+
+    }
+    setId(id){
+        this.id=id;
+    }
+    setName(name){
+        this.pokeName= name;
+    }
+    setType(type){
+        this.type.push(type);
+    }
+
+    setWeaknesses(weakness){
+        this.weaknesses.push(weakness);
+    }
+    setEvoutions(evolution){
+        this.evolutions.push(evolution);
+    }
+
+}
 
 switch (option2)
 {
@@ -17,14 +53,18 @@ switch (option2)
     break;
     
     case 'create':
-    let newPokemon =
-    {
-        id: 2,
-        name: getInfo3,
-        type: [],
-        weakness: [],
-        evolutions: []
-    }
+    let newPokemon = new Pokemon ({id: 1,pokeName: "Charizard",});
+    newPokemon.setType("fire");
+    newPokemon.setType("flying");
+    newPokemon.setWeaknesses("water");
+    newPokemon.setWeaknesses("electric");
+    newPokemon.setWeaknesses("rock");
+    newPokemon.setEvoutions("perrito");
+    
+
+
+    console.log(newPokemon);
+
     saveNewPokemon(newPokemon)
     console.log ('Sucefully added to the Pokedex')
     break;
@@ -36,3 +76,4 @@ switch (option2)
     default: console.log ('No entiendo que necesitas, te recuerdo las opciones: ' + "\n" + '-------------------------' + "\n" + '1) listar: Visualizar todas las tareas actuales' + "\n" + '2) crear: Ingresar un nuevo titulo con estado pendiente' + "\n" + '3) filtrar: Visualizar las tareas actuales segun el estado ingresado')
     break;
 }
+
